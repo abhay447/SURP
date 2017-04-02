04314cec7ed422be4d67d293a0769556aad379b1
Abhay Chatrvedi <abhay447@gmail.com>
2017-04-03 02:23:09 +0530

    Added search/exit options

--- SURP.py
+++ SURP.py
@@ -17,7 +17,7 @@ def getWordCounts(docId,pdf):
     pdfReader = PyPDF2.PdfFileReader(open(pdf, 'rb'))
     wordList = []
     for i in range(pdfReader.getNumPages()):
-        msg = "Reading Document: %i at Page %i of %i"%(docId+1,i,pdfReader.getNumPages()) 
+        msg = "Reading Document: %i of %i at Page %i of %i"%(docId+1,len(docList),i,pdfReader.getNumPages()) 
         ConsoleOut(msg)
         pageObj = pdfReader.getPage(i)
         pageText = pageObj.extractText()
@@ -66,57 +66,60 @@ def LoadData():
     except ValueError as e:
         print e.args[0]
         scanFolder()
-        
-    
+
+
 stemmer = SnowballStemmer("english")#PorterStemmer()
 docList = glob.glob('./*.pdf')
 wordDict = {}
 LoadData()
 
-query = raw_input("\nEnter your query :")
-query = re.compile('\w+').findall(query.encode('ascii', 'ignore'))
-q = [stemmer.stem(i) for i in query]
+loop = "a"
+while loop!="exit":
+    query = raw_input("\nEnter your query :")
+    query = re.compile('\w+').findall(query.encode('ascii', 'ignore'))
+    q = [stemmer.stem(i) for i in query]
 
-#term frequecy for query
-tf = Counter(q)
+    #term frequecy for query
+    tf = Counter(q)
 
-#inverse document frequency for query
-idf = {}
-for a in tf:
-    if a in wordDict:
-        idf[a] = len(docList)/len(wordDict[a]) if len(wordDict[a])>0 else 0
-    else:
-        idf[a] = 0
-
-#weighing the query terms
-q_wts = {}
-for a in tf:
-    q_wts[a] = (1+math.log10(tf[a]))*idf[a]
-q_wts = normalize(q_wts)
-
-#term frequency for query terms in docs
-doc_cts = {}
-for i in range(len(docList)):
-    doc_cts[i] = {}
+    #inverse document frequency for query
+    idf = {}
     for a in tf:
-        if not a in wordDict:
-            doc_cts[i][a] = 0
-        elif i not in wordDict[a]:
-            doc_cts[i][a] = 0
+        if a in wordDict:
+            idf[a] = len(docList)/len(wordDict[a]) if len(wordDict[a])>0 else 0
         else:
-            doc_cts[i][a] = wordDict[a][i]
+            idf[a] = 0
 
-#document matching
-doc_wt = {}
-for i in doc_cts:
-    doc_wt[docList[i]] = 0
-    doc_cts[i] = normalize(doc_cts[i]) #normalize doc tf
+    #weighing the query terms
+    q_wts = {}
     for a in tf:
-        doc_wt[docList[i]] += doc_cts[i][a]*q_wts[a] #cosine of query weights and doc tf
+        q_wts[a] = (1+math.log10(tf[a]))*idf[a]
+    q_wts = normalize(q_wts)
 
-#Print Results in sorted order
-doc_wt = sorted(doc_wt.items(), key=operator.itemgetter(1), reverse=True)
-print "Ranked Matches:\n================" 
-for a in doc_wt:
-    if a[1]>0:
-        print a[0]
+    #term frequency for query terms in docs
+    doc_cts = {}
+    for i in range(len(docList)):
+        doc_cts[i] = {}
+        for a in tf:
+            if not a in wordDict:
+                doc_cts[i][a] = 0
+            elif i not in wordDict[a]:
+                doc_cts[i][a] = 0
+            else:
+                doc_cts[i][a] = wordDict[a][i]
+
+    #document matching
+    doc_wt = {}
+    for i in doc_cts:
+        doc_wt[docList[i]] = 0
+        doc_cts[i] = normalize(doc_cts[i]) #normalize doc tf
+        for a in tf:
+            doc_wt[docList[i]] += doc_cts[i][a]*q_wts[a] #cosine of query weights and doc tf
+
+    #Print Results in sorted order
+    doc_wt = sorted(doc_wt.items(), key=operator.itemgetter(1), reverse=True)
+    print "Ranked Matches:\n================" 
+    for a in doc_wt:
+        if a[1]>0:
+            print a[0]
+    loop = raw_input("Type exit to quit or press enter to continue \n")
